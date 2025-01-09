0e9c43a8ec2b89596c928af16397f590766e1f2d
erarunkushwaha <er.arunkushwaha@gmail.com>
2025-01-09 15:01:58 +0530

    tailwind added to userapp

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+import './globals.css';
+
+
+export default function Home() {
+  return (
+    <div className="text-3xl text-red-500">
+     Hello there
+    </div>
+  );
+}
---
+++ apps/user-app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/**/*.{html,js,jsx,tsx}","./components/**/*.{html,js,jsx,tsx}","./src/**/*.{html,js,jsx,tsx}","../../packages/ui/./src/**/*.{html,js,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
